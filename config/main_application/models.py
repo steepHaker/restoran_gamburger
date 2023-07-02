1df06ab974a8548da74f347bfddb391a5214537b
steepHaker <andrejmagkoev585@gmail.com>
2023-07-02 03:39:03 +0300

    Update models.py

--- config/main_application/models.py
+++ config/main_application/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+# from mptt.models import MPTTModel, TreeForeignKey
 
 class Category(models.Model):
-    title = models.CharField('Описание категории меню',max_length=1000, default="")
     name = models.CharField('Названиие категории', max_length=255, default="")
     image = models.ImageField('Иконка', upload_to='category_images/')
     slug = models.SlugField()
+    # parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+    
+    # class MPTTMeta:
+    #     order_insertion_by = ['name']
     
 
     def __str__(self):
@@ -22,9 +26,9 @@ class Dish(models.Model):
     name = models.CharField('Названиие блюда', max_length=255)
     description = models.TextField('Описание блюда, состав', max_length=500)
     CURRENCY_CHOICES = (
-        ('RUB', '₽'),
-        ('USD', '$'),
-        ('EUR', '€'),
+        ('₽', 'RUB'),
+        ('$', 'USD'),
+        ('€', 'EUR'),
     )
     price = models.DecimalField('Цена', max_digits=10, decimal_places=2)
     image = models.ImageField('Фото блюда', upload_to='dishs/', default="")
@@ -33,56 +37,16 @@ class Dish(models.Model):
 
     def __str__(self):
         return self.name
-
-
-
-
-# from django.db import models
-
-# class Category(models.Model):
-#     title = models.CharField(max_length=255, default="")
-#     name = models.CharField(max_length=255, default="")
-#     image = models.ImageField(upload_to='category_images/')
-#     slug = models.SlugField()
     
 
-#     def __str__(self):
-#         return self.name
-    
-# class UpperSlider(models.Model):
-#     image = models.ImageField(upload_to='slider_images/')
-#     description = models.TextField(unique=True, default="")
-#     related_category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
+class DishTitle(models.Model):
+    title = models.CharField('Описание категории меню',max_length=1000, default="")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
-#     def __str__(self):
-#         return self.description
+    def __str__(self):
+        return self.name
 
-
-
-# class DishTitle(models.Model):
-#     title = models.CharField(max_length=255, default="")
-#     category = models.ForeignKey(Category, on_delete=models.CASCADE)#category = models.ForeignKey(Category, on_delete=models.CASCADE)
-
-#     def __str__(self):
-#         return self.title
-
-
-
-# class Dish(models.Model):
-#     name = models.CharField(max_length=255)
-#     description = models.TextField(max_length=500)
-#     CURRENCY_CHOICES = (
-#         ('RUB', '₽'),
-#         ('USD', '$'),
-#         ('EUR', '€'),
-#     )
-#     price = models.DecimalField(max_digits=10, decimal_places=2)
-#     image = models.ImageField(upload_to='dishs/', default="")
-#     currency = models.CharField(max_length=3, choices=CURRENCY_CHOICES, default="")
+# class Menu(models.Model):
+#     name = models.CharField('Имя категории', max_length=50)
+#     image = models.ImageField('Иконка', upload_to='menu_icon')
 #     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-
-#     def __str__(self):
-#         return self.name
-
-
-

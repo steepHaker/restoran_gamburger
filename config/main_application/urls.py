f17b1359a75fbaaa6f7975fb36a13df4e3ee9467
steepHaker <andrejmagkoev585@gmail.com>
2023-07-05 16:03:48 +0300

    Update urls.py

--- config/main_application/urls.py
+++ config/main_application/urls.py
@@ -1,8 +1,16 @@
+from django.shortcuts import redirect
 from django.urls import path
-from . import views
+from . import views 
 
-urlpatterns = [ 
-    path('', views.HomeView.as_view(), name='home'),
-    path('<slug:slug>/', views.MenuView.as_view(), name="dish"),
-    
+
+
+urlpatterns = [  
+    path('', views.HomeMyView.as_view(), name='home'),
+    # Добавьте следующую строку для перенаправления /home/ на homeMain.html
+    #path('home/', views.HomeMyView.as_view(), name=''),
+    path('<slug:slug>/', views.HomeMyView.as_view(), name="home-main"),
+    # path('', views.HomeMyView.as_view(), name='main/homeMain.html'),
+    # path('', views.MenuView.as_view(), name='menu'),
+    # path('menu/<slug:slugPage>/', views.MenuView.as_view(), name='menu'),
+    # path('pages/', views.PageView.as_view(), name='page'),
 ]

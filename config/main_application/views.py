d86d12006aafbce9dc01fddb4f2aaf3436802d2b
steepHaker <andrejmagkoev585@gmail.com>
2023-07-05 22:23:54 +0300

    Update views.py

--- config/main_application/views.py
+++ config/main_application/views.py
@@ -1,38 +1,51 @@
+from multiprocessing import context
+from pickle import NONE
 from urllib import request
 from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import redirect
 from django.views.generic import ListView
 from .models import Dish, InfoBanner, UpperSlider, Category, Page, Logo
 
-
-class HomeMyView(ListView):
+class BaseView(ListView):
     model = Dish
-    paginate_by = 10
-    template_name = "main/homeMain.html"
+    template_name = NONE
     page_slug = "home"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['upper_slider'] = UpperSlider.objects.get(related_Page__slug=self.page_slug)
+        
+        try:
+            context['upper_slider'] = UpperSlider.objects.get(related_Page__slug=self.page_slug)
+        except UpperSlider.DoesNotExist:
+            context['upper_slider'] = None
+
         context['category_name'] = Category.objects.all()
         context['category_dish'] = Category.objects.prefetch_related('dish_set').all()
         context['pages'] = Page.objects.all()
         context['icons'] = Logo.objects.all()
+        
         return context
 
-    
-class MenuView(HomeMyView):
-    template_name = 'main/menu.html'
+
+
+
+
+class HomeView(BaseView):
+    template_name = 'main/homeMain.html'
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['banners'] = InfoBanner.objects.all()
         return context
+    
 
-    def get(self, request, *args, **kwargs):
-        if request.path == '/home/':
-            return redirect('home')
+class MenuView(BaseView):
+    template_name = 'main/menu.html'
 
-        return super().get(request, *args, **kwargs)
+    
+   
+    
+
+
 
 

2fede7cf342e7aa1849b0ca09d4027f8df983ea4
steepHaker <andrejmagkoev585@gmail.com>
2023-07-05 23:36:58 +0300

    Update urls.py

--- config/main_application/urls.py
+++ config/main_application/urls.py
@@ -7,5 +7,6 @@ from . import views
 urlpatterns = [  
     path('', views.HomeView.as_view(), name='home'),
     path('menu/', views.MenuView.as_view(), name='menu'),
-    path('home/', views.HomeView.as_view(), name='home'),
+    path('home/', views.HomeView.as_view(), name=''),
+    path('<slug:slug>/', views.PageDetailView.as_view(), name='page-detail'),
 ]

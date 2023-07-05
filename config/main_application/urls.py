bef6ba541eb8b03d3681549b35f900f3fe22cb9e
steepHaker <andrejmagkoev585@gmail.com>
2023-07-05 22:23:52 +0300

    Update urls.py

--- config/main_application/urls.py
+++ config/main_application/urls.py
@@ -5,12 +5,7 @@ from . import views
 
 
 urlpatterns = [  
-    path('', views.HomeMyView.as_view(), name='home'),
-    # Добавьте следующую строку для перенаправления /home/ на homeMain.html
-    #path('home/', views.HomeMyView.as_view(), name=''),
-    path('<slug:slug>/', views.HomeMyView.as_view(), name="home-main"),
-    # path('', views.HomeMyView.as_view(), name='main/homeMain.html'),
-    # path('', views.MenuView.as_view(), name='menu'),
-    # path('menu/<slug:slugPage>/', views.MenuView.as_view(), name='menu'),
-    # path('pages/', views.PageView.as_view(), name='page'),
+    path('', views.HomeView.as_view(), name='home'),
+    path('menu/', views.MenuView.as_view(), name='menu'),
+    path('home/', views.HomeView.as_view(), name='home'),
 ]

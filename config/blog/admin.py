bb745a3b612823fdc4dbfd07ce6772674c7581b6
steepHaker <andrejmagkoev585@gmail.com>
2023-07-07 02:39:09 +0300

    Update admin.py

--- config/blog/admin.py
+++ config/blog/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog import models
+from blog.models import Post, Category
+
+admin.site.register(models.Tag)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('short_title', 'create_at')
+    save_as = True
+    save_on_top = True
+
+    def short_title(self, obj):
+        if len(obj.title) > 20:
+            return obj.title[:20] + '...'  # Сокращение текста до 20 символов
+        else:
+            return obj.title
+
+    short_title.short_description = 'Title'  # Краткое описание поля
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(models.Category)

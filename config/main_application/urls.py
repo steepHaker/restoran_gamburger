8f7c3b458ca160bfd69807605f7528a61804282a
steepHaker <andrejmagkoev585@gmail.com>
2023-07-07 02:38:51 +0300

    Update urls.py

--- config/main_application/urls.py
+++ config/main_application/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import include, path
 from main_application import views
 
 urlpatterns = [
     path('home/', views.HomeView.as_view(), name='home'),
     path('home/menu/', views.MenuView.as_view(), name='menu'),
+    path('', include('blog.urls')),
+    
 ]

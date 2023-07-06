770bb4315f4790511ef604a219bfe993a40a21f5
steepHaker <andrejmagkoev585@gmail.com>
2023-07-06 17:23:37 +0300

    Update urls.py

--- config/main_application/urls.py
+++ config/main_application/urls.py
@@ -1,12 +1,7 @@
-from django.shortcuts import redirect
 from django.urls import path
-from . import views 
+from main_application import views
 
-
-
-urlpatterns = [  
-    path('', views.HomeView.as_view(), name='home'),
-    path('menu/', views.MenuView.as_view(), name='menu'),
-    path('home/', views.HomeView.as_view(), name=''),
-    path('<slug:slug>/', views.PageDetailView.as_view(), name='page-detail'),
+urlpatterns = [
+    path('home/', views.HomeView.as_view(), name='home'),
+    path('home/menu/', views.MenuView.as_view(), name='menu'),
 ]

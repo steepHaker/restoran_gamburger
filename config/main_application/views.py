aab9f81fb8830615d7e26825fb43ee5cd3f3fa79
steepHaker <andrejmagkoev585@gmail.com>
2023-07-06 17:23:49 +0300

    Update views.py

--- config/main_application/views.py
+++ config/main_application/views.py
@@ -1,57 +1,43 @@
-from multiprocessing import context
-from pickle import NONE
-from urllib import request
-from django.core.exceptions import ObjectDoesNotExist
-from django.shortcuts import redirect
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView
 from .models import Dish, InfoBanner, UpperSlider, Category, Page, Logo
 
 class BaseView(ListView):
     model = Dish
-    template_name = NONE
+    template_name = 'base.html'
     page_slug = "home"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        
-        try:
-            context['upper_slider'] = UpperSlider.objects.get(related_Page__slug=self.page_slug)
-        except UpperSlider.DoesNotExist:
-            context['upper_slider'] = None
-
+        context['upper_slider'] = UpperSlider.objects.get(related_Page__slug='home')
         context['category_name'] = Category.objects.all()
         context['category_dish'] = Category.objects.prefetch_related('dish_set').all()
         context['pages'] = Page.objects.all()
         context['icons'] = Logo.objects.all()
-        
         return context
-
-
-
-
 
 class HomeView(BaseView):
     template_name = 'main/homeMain.html'
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['banners'] = InfoBanner.objects.all()
+        try:
+            current_page = Page.objects.get(slug='home')
+            context['upper_slider'] = UpperSlider.objects.get(related_Page=current_page)
+        except (Page.DoesNotExist, UpperSlider.DoesNotExist):
+            context['upper_slider'] = None
         return context
     
-
 class MenuView(BaseView):
-    template_name = 'main/menu.html'
-
-
-class PageDetailView(DetailView):
-    model = Page
     template_name = 'main/page_detail.html'
-    context_object_name = 'page'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        try:
+            current_page = Page.objects.get(slug='menu')
+            context['upper_slider'] = UpperSlider.objects.get(related_Page=current_page)
+        except (Page.DoesNotExist, UpperSlider.DoesNotExist):
+            context['upper_slider'] = None
+        return context
     
-   
-    
-
-
-
 

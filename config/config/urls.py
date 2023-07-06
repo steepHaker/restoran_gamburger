6b748b1487a3295ba9639e098da86de299e165f6
steepHaker <andrejmagkoev585@gmail.com>
2023-07-07 02:39:06 +0300

    Update urls.py

--- config/config/urls.py
+++ config/config/urls.py
@@ -1,3 +1,4 @@
+import debug_toolbar
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
@@ -8,8 +9,11 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main_application.urls')),
+    path('__debug__/', include(debug_toolbar.urls)),
     path('', include('blog.urls')),
 ]
+
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
